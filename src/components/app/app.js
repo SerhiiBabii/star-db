8935088163c6685ad0f971b2fc2168f0f7d1e508
Serhii Babii <babiysergey1988@gmail.com>
2019-09-20 20:53:51 +0300

    Added button ToggleRandomPlanet

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import Headrer from "../header";
 import RandomPlanet from "../random-planet";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 
 import "./app.css";
+import ErrorIndicator from "../error-indicator";
 
-const App = () => {
-  return (
-    <div>
-      <Headrer />
-      <RandomPlanet />
-      <div className="rew mb2">
-        <ItemList />
-      </div>
-      <div>
-        <PersonDetails />
-      </div>
-    </div>
-  );
-};
+export default class App extends Component {
+  state = {
+    selectedPerson: null,
+    showRandomPlanet: true,
+    hasError: false
+  };
 
-export default App;
+  componentDidCatch() {
+    console.log("componentDidCatch");
+    this.setState({
+      hasError: true
+    });
+  }
+
+  onPersonSelected = id => {
+    this.setState({
+      selectedPerson: id
+    });
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState(state => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      };
+    });
+  };
+
+  render() {
+    if (this.hasError) {
+      return <ErrorIndicator />;
+    }
+
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+    return (
+      <div className="stardb-app">
+        <Headrer />
+        {planet}
+
+        <button
+          className="toggle-planet btn btn-warning btn-lg"
+          onClick={this.toggleRandomPlanet}
+        >
+          Toggle Random Planet
+        </button>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
